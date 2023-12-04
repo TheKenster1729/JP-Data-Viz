31d0a04b55aab8bc0fc57cc81055d18ee225049b
TheKenster1729 <kenny.coxgm@gmail.com>
2023-12-04 13:03:31 -0500

    changed port and ip

--- app.py
+++ app.py
@@ -408,7 +408,9 @@ def update_figure(output, region, scenario, year):
 ###############################################
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = True,
+            port = "8050",
+            host = "18.13.51.101")
 
     # discarded components
 

0e2b63eef256ad9bcdab8bd7e23cc01620f05146
Morgan Ludwig <mjbludwig@gmail.com>
2023-12-22 13:15:41 -0500

    changed to port 8060 for the dev branch so I can run dev and prod at the same time on svante4

--- app.py
+++ app.py
@@ -408,7 +408,7 @@ def update_figure(output, region, scenario, year):
 ###############################################
 
 if __name__ == '__main__':
-    app.run(debug = True, host="0.0.0.0")
+    app.run(debug = True, host="0.0.0.0", port=8060)
 
     # discarded components
 
